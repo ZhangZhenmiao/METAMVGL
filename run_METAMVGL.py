fb971e7784e588e6fc66f11d176b7aa8546c59fd
Zhenmiao Zhang <zhz142@ucsd.edu>
2023-06-04 09:35:38 +0800

    enable arbitrary initial binning tools for wrapper through parameter -br

--- run_METAMVGL.py
+++ run_METAMVGL.py
@@ -36,6 +36,13 @@ parser.add_argument(
     help="the initial binning tool to create initial binning results (choose from MetaBat2 and MaxBin2, default MetaBat2)",
 )
 parser.add_argument(
+    "-br",
+    "--initial_binning_result",
+    required=False,
+    default=None,
+    help="the result dir of arbitrary initial binning tool (-b is ignored if this parameter is set)",
+)
+parser.add_argument(
     "-r1",
     "--reads1",
     required=True,
@@ -133,7 +140,13 @@ if args.assembler.lower() == "metaspades" and args.paths is None:
 # check initial binning tool
 if args.initial_binning.lower() not in ("maxbin2", "metabat2"):
     logging(
-        "Please check the --initial_binning or -i, which should be MetaBat2 or MaxBin2."
+        "Please check the --initial_binning or -b, which should be MetaBat2 or MaxBin2."
+    )
+    sys.exit(1)
+
+if args.initial_binning_result is not None and not os.path.isdir(args.initial_binning_result):
+    logging(
+        "Please check the --initial_binning_result or -br, which should be an output directory of a initial binning tool."
     )
     sys.exit(1)
 
@@ -164,75 +177,78 @@ else:
 # initial binning
 bindir = ""
 bamfile = ""
-if args.initial_binning.lower() == "maxbin2":
-    run_cmd(["mkdir", f"{args.output}/MaxBin2"], "mkdir")
-    run_cmd(
-        [
-            "run_MaxBin.pl",
-            "-contig",
-            f"{args.contigs}",
-            "-reads",
-            args.reads1,
-            "-reads2",
-            args.reads2,
-            "-out",
-            f"{args.output}/MaxBin2/bin",
-            "-thread",
-            str(args.threads),
-        ],
-        "MaxBin2",
-    )
-    bindir = f"{args.output}/MaxBin2"
+if args.initial_binning_result is None:
+    if args.initial_binning.lower() == "maxbin2":
+        run_cmd(["mkdir", f"{args.output}/MaxBin2"], "mkdir")
+        run_cmd(
+            [
+                "run_MaxBin.pl",
+                "-contig",
+                f"{args.contigs}",
+                "-reads",
+                args.reads1,
+                "-reads2",
+                args.reads2,
+                "-out",
+                f"{args.output}/MaxBin2/bin",
+                "-thread",
+                str(args.threads),
+            ],
+            "MaxBin2",
+        )
+        bindir = f"{args.output}/MaxBin2"
+    else:
+        run_cmd(["mkdir", f"{args.output}/MetaBat2"], "mkdir")
+        logging("Create bam file starts.")
+        if not os.path.isfile(f"{args.contigs}.bwt"):
+            subprocess.run(["bwa", "index", args.contigs])
+        pipe = subprocess.Popen(
+            ["bwa", "mem", "-t", str(args.threads), args.contigs, args.reads1, args.reads2],
+            stdout=subprocess.PIPE,
+        )
+        subprocess.check_output(
+            [
+                "samtools",
+                "sort",
+                "-@",
+                str(args.threads),
+                "-",
+                "-o",
+                f"{args.output}/MetaBat2/align.megahit.bam",
+            ],
+            stdin=pipe.stdout,
+        )
+        pipe.communicate()
+        logging("Create bam file ends.")
+        run_cmd(
+            [
+                "jgi_summarize_bam_contig_depths",
+                "--outputDepth",
+                f"{args.output}/MetaBat2/align.megahit.depth",
+                f"{args.output}/MetaBat2/align.megahit.bam",
+            ],
+            "Metabat2_bam_depth",
+        )
+        run_cmd(
+            [
+                "metabat2",
+                "-i",
+                args.contigs,
+                "-a",
+                f"{args.output}/MetaBat2/align.megahit.depth",
+                "-m",
+                str(1500),
+                "-o",
+                f"{args.output}/MetaBat2/bin",
+                "-t",
+                str(args.threads),
+            ],
+            "Metabat2_main",
+        )
+        bindir = f"{args.output}/MetaBat2"
+        bamfile = f"{args.output}/MetaBat2/align.megahit.bam"
 else:
-    run_cmd(["mkdir", f"{args.output}/MetaBat2"], "mkdir")
-    logging("Create bam file starts.")
-    if not os.path.isfile(f"{args.contigs}.bwt"):
-        subprocess.run(["bwa", "index", args.contigs])
-    pipe = subprocess.Popen(
-        ["bwa", "mem", "-t", str(args.threads), args.contigs, args.reads1, args.reads2],
-        stdout=subprocess.PIPE,
-    )
-    subprocess.check_output(
-        [
-            "samtools",
-            "sort",
-            "-@",
-            str(args.threads),
-            "-",
-            "-o",
-            f"{args.output}/MetaBat2/align.megahit.bam",
-        ],
-        stdin=pipe.stdout,
-    )
-    pipe.communicate()
-    logging("Create bam file ends.")
-    run_cmd(
-        [
-            "jgi_summarize_bam_contig_depths",
-            "--outputDepth",
-            f"{args.output}/MetaBat2/align.megahit.depth",
-            f"{args.output}/MetaBat2/align.megahit.bam",
-        ],
-        "Metabat2_bam_depth",
-    )
-    run_cmd(
-        [
-            "metabat2",
-            "-i",
-            args.contigs,
-            "-a",
-            f"{args.output}/MetaBat2/align.megahit.depth",
-            "-m",
-            str(1500),
-            "-o",
-            f"{args.output}/MetaBat2/bin",
-            "-t",
-            str(args.threads),
-        ],
-        "Metabat2_main",
-    )
-    bindir = f"{args.output}/MetaBat2"
-    bamfile = f"{args.output}/MetaBat2/align.megahit.bam"
+    bindir = args.initial_binning_result
 if args.assembler.lower() == "metaspades":
     run_cmd(
         [
